dd654e704ebda0fb641c8884df9be9f57e0360d7
olsanx <121573851+olsanx@users.noreply.github.com>
2024-09-02 18:12:31 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -60,11 +60,20 @@ animateScroll();
 const cvButton = document.getElementById('cv-button');
 const button2 = document.getElementById('more-button');
 
-cvButton.addEventListener('touchstart', () => {
-  const link = document.createElement('a');
-  link.href ='https://drive.google.com/uc?export=download&id=1h2QeuWLlSVPBYezUdbn2fjHBqz33T5tn';
-  link.download = 'JasonsPortfolio.pdf';
-  link.click();
+cvButton.addEventListener('click', () => {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://drive.google.com/uc?export=download&id=1h2QeuWLlSVPBYezUdbn2fjHBqz33T5tn', true);
+  xhr.responseType = 'blob';
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      const blob = new Blob([xhr.response], { type: 'application/pdf' });
+      const link = document.createElement('a');
+      link.href = URL.createObjectURL(blob);
+      link.download = 'JasonsPortfolio.pdf';
+      link.click();
+    }
+  };
+  xhr.send();
 });
 
 button2.addEventListener('click', () => {
